f29152019776c7a5803bc284ec25cd5cf81707a4
Abdullah Bin Nasir <a.dshamsi2207@gmail.com>
2025-09-04 14:56:01 +0500

    First Commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -231,12 +231,12 @@ export const mySocials = [
   },
   {
     name: "Linkedin",
-    href: "https://www.linkedin.com/in/ali-sanati/",
+    href: "https://www.linkedin.com/in/abdullah-nasir-289763292/",
     icon: "/assets/socials/linkedIn.svg",
   },
   {
     name: "Instagram",
-    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    href: "https://www.instagram.com/devnest_.dev/",
     icon: "/assets/socials/instagram.svg",
   },
 ];
